514b4cc124e79eaf284fd03d8ebd4c38210983e6
stevenmjl <stevenmjl.24@gmail.com>
2024-05-13 18:00:44 -0400

    Recibiendo el contexto.

---
+++ RegistroTecnicos/Services/TecnicosServices.cs
@@ -0,0 +1,15 @@
+﻿using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services
+{
+    public class TecnicosServices
+    {
+        private readonly Contexto _context;
+        public TecnicosServices(Contexto contexto)
+        {
+            _context = contexto;
+        }
+    }
+}
