17f74ba7e426042665ce97d520fd025ce7a5c47e
stevenmjl <stevenmjl.24@gmail.com>
2024-05-13 15:48:57 -0400

    Creación de modelo Tecnicos.

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+        public string? Nombres { get; set; }
+        public float SueldoHora { get; set; }
+    }
+}
