aef4ee4f2d461429c1d03faa6d642cabdc480af9
stevenmjl <stevenmjl.24@gmail.com>
2024-05-25 20:34:52 -0400

    Agregando requerimientos a Tecnicos.

--- RegistroTecnicos/Models/Tecnico.cs
+++ RegistroTecnicos/Models/Tecnico.cs
@@ -1,12 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RegistroTecnicos.Models
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
 {
-    public class Tecnico
-    {
-        [Key]
-        public int TecnicoId { get; set; }
-        public string? Nombres { get; set; }
-        public float SueldoHora { get; set; }
-    }
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [Required(ErrorMessage = "Este campo debe ser llenado con los datos requeridos")]
+    [RegularExpression(@"[a-zA-ZñÑ\s]+$", ErrorMessage = "Este campo no debe contener caracteres especiales")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Este campo debe ser llenado con los datos requeridos")]
+    [Range(0.1, 100000000, ErrorMessage = "Ingrese un sueldo mayor a 0 y menor a 100,000,000")]
+    public decimal SueldoHora { get; set; }
 }
