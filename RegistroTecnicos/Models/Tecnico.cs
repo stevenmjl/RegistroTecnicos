be1dbdba4954abce21efcbc25df18742af9dd4d7
stevenmjl <stevenmjl.24@gmail.com>
2024-06-05 12:04:46 -0400

    Arreglando modelo Tecnico.

--- RegistroTecnicos/Models/Tecnico.cs
+++ RegistroTecnicos/Models/Tecnico.cs
@@ -7,11 +7,11 @@ public class Tecnicos
     [Key]
     public int TecnicoId { get; set; }
 
-    [Required(ErrorMessage = "Este campo debe ser llenado con los datos requeridos")]
+    [Required(ErrorMessage = "Se debe agregar el nombre completo.")]
     [RegularExpression(@"[a-zA-ZñÑ\s]+$", ErrorMessage = "Este campo no debe contener caracteres especiales")]
-    public string? Nombres { get; set; }
+    public string Nombre { get; set; } = "Nombre";
 
-    [Required(ErrorMessage = "Este campo debe ser llenado con los datos requeridos")]
+    [Required(ErrorMessage = "Debe tener un sueldo correcto.")]
     [Range(0.1, 100000000, ErrorMessage = "Ingrese un sueldo mayor a 0 y menor a 100,000,000")]
     public decimal SueldoHora { get; set; }
 }
