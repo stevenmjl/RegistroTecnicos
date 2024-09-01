3b15d7aab721ae87ceb79d2851f5de4944f16a4a
stevenmjl <stevenmjl.24@gmail.com>
2024-09-01 15:15:42 -0400

    Agregando modelo Tecnicos.

---
+++ Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [Required(ErrorMessage = "Se debe agregar el nombre completo.")]
+    [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚàâçèéêëîïôûùüÿÀÂÇÈÉÊËÎÏÔÛÙÜŸ\s]+$",
+    ErrorMessage = "Este campo no debe contener números ni caracteres especiales.")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Debe tener un sueldo correcto.")]
+    [Range(0.01, 10000000, ErrorMessage = "Ingrese un sueldo mayor a 0 y menor a 10,000,000")]
+    public double SueldoHora { get; set; }
+}
