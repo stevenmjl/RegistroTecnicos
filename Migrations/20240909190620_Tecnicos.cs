d84248103546bf3ddb37e81337ea1377320fccb0
stevenmjl <stevenmjl.24@gmail.com>
2024-09-09 15:09:54 -0400

    Corrección de nombre tabla, Tecnico a Tecnicos.

---
+++ Migrations/20240909190620_Tecnicos.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tecnicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tecnico",
+                table: "Tecnico");
+
+            migrationBuilder.RenameTable(
+                name: "Tecnico",
+                newName: "Tecnicos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tecnicos",
+                table: "Tecnicos",
+                column: "TecnicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tecnicos",
+                table: "Tecnicos");
+
+            migrationBuilder.RenameTable(
+                name: "Tecnicos",
+                newName: "Tecnico");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tecnico",
+                table: "Tecnico",
+                column: "TecnicoId");
+        }
+    }
+}
