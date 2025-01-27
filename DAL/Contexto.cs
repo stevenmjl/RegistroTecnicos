f14958f6c1a5ca52436ab8d9c9c2ca815c0a54ab
stevenmjl <stevenmjl.24@gmail.com>
2025-01-27 19:11:34 -0400

    Agregando Ciudades al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,4 +8,6 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
     public DbSet<Tecnicos> Tecnicos { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Ciudades> Ciudades { get; set; }
+
 }
